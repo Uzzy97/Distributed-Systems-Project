1b0fa11d43a1a3c41da264a09b45177dc1e84e93
Usman Sattar <G00345816@gmit.ie>
2019-11-26 14:31:59 +0000

    User Account Service

---
+++ grpcClient/src/main/java/ie/gmit/ds/User.java
@@ -0,0 +1,40 @@
+package ie.gmit.ds;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+	int userId;
+	String userName;
+	String email;
+	String password;
+	
+	@JsonProperty
+	public int getUserId() {
+		return userId;
+	}
+
+	@JsonProperty
+	public String getUserName() {
+		return userName;
+	}
+
+	@JsonProperty
+	public String getEmail() {
+		return email;
+	}
+
+	@JsonProperty
+	public String getPassword() {
+		return password;
+	}
+
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public User(int userId, String userName, String email, String password) {
+		
+		
+	}
+}
